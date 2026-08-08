f80c5221a74c4b6bad1620de1756f676e651f91c
aa89976566 <aa89976566@gmail.com>
2026-08-08 14:58:24 +0200

    Bust cache for yellow side icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={asset("/haodada-site-v15.css")} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=yellow-icons-1`} />
       </head>
       <body>{children}</body>
     </html>
