fbd9b8282a21dd803ba5a82448f5ad1a463ade9d
aa89976566 <aa89976566@gmail.com>
2026-08-10 22:55:00 +0200

    Bust cache for Taiwan retro title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=video-lazy-1`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=taiwan-retro-title-1`} />
       </head>
       <body>{children}</body>
     </html>
