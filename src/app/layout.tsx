134ea12b63faae630021df4eba49b22f1f061464
aa89976566 <aa89976566@gmail.com>
2026-08-11 19:49:28 +0200

    Bust cache for sticky printer fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=scroll-printer-v8`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=scroll-printer-v8b`} />
       </head>
       <body>{children}</body>
     </html>
