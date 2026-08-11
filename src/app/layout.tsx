3e4911fab4c8c31559127cd34041f9190d9312fe
aa89976566 <aa89976566@gmail.com>
2026-08-11 04:55:31 +0200

    Bust cache for hero order screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=title-reverted-1`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=hero-order-screen-v7`} />
       </head>
       <body>{children}</body>
     </html>
