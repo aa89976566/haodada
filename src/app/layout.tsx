3cb9fbbbaa3ae26ebaaacc77dab6457e768f8998
aa89976566 <aa89976566@gmail.com>
2026-08-12 16:40:25 +0200

    Bust cache for enlarged product report

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=product-report-v12`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=product-report-v13`} />
       </head>
       <body>{children}</body>
     </html>
