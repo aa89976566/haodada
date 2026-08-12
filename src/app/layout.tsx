32bad485aadb97bf2cb407f51c2796a9d6b48ebc
aa89976566 <aa89976566@gmail.com>
2026-08-12 17:03:38 +0200

    Bust cache for tear-drop animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=product-report-v14`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=tear-drop-v15`} />
       </head>
       <body>{children}</body>
     </html>
