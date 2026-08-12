65be6c3ffbda5ff5cb7ebadef6f873ab4070478d
aa89976566 <aa89976566@gmail.com>
2026-08-12 17:25:33 +0200

    Redeploy outer side report animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=outer-side-report-v16`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=outer-side-report-v16b`} />
       </head>
       <body>{children}</body>
     </html>
