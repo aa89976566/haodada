3875812a570b2f40b4ac9eab5ef41ebb31a13504
aa89976566 <aa89976566@gmail.com>
2026-08-12 13:41:42 +0200

    Bust cache for front-facing printer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=scroll-printer-v8c`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=front-printer-v10`} />
       </head>
       <body>{children}</body>
     </html>
