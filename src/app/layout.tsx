711be32a7617ddeb318d58f0c4cb8021dba3588b
aa89976566 <aa89976566@gmail.com>
2026-08-14 15:59:53 +0200

    Bust cache for food receipt section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=cards-removed-v18`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=food-receipt-v19`} />
       </head>
       <body>{children}</body>
     </html>
