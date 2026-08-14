d700ba118fd3e363300f16515aa0154fc1521b89
aa89976566 <aa89976566@gmail.com>
2026-08-14 17:19:57 +0200

    Bust cache for compact receipt grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=natural-chat-gap-v25`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=compact-receipt-grid-v28`} />
       </head>
       <body>{children}</body>
     </html>
