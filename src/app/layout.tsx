a18ae3154c5bf0ecbb6e776dcfd16e0c1026c556
aa89976566 <aa89976566@gmail.com>
2026-08-15 22:16:51 +0200

    Refresh framed expiry receipt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=chat-photo-replace-v36`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=sectioned-expiry-v37`} />
       </head>
       <body>{children}</body>
     </html>
