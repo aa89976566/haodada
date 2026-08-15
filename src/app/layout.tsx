3139a161f4a01188322b9a1a47a933a657bd4a2e
aa89976566 <aa89976566@gmail.com>
2026-08-15 08:24:05 +0200

    Bust cache for extended receipt grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=receipt-slot-aligned-v34`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=receipt-grid-extension-v35`} />
       </head>
       <body>{children}</body>
     </html>
