dc35cbdb0ca587a4a47fae3e461e481cf1f41d21
aa89976566 <aa89976566@gmail.com>
2026-08-15 22:32:12 +0200

    Refresh global cursor removal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=hide-print-cursor-v38`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=hide-site-cursor-v39`} />
       </head>
       <body>{children}</body>
     </html>
