92592d054602a206c9284cf1599e51cd26c339f4
aa89976566 <aa89976566@gmail.com>
2026-08-16 10:22:33 +0200

    更新左右背景融合樣式版本

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=side-copy-fix-v45`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=side-background-blend-v46`} />
       </head>
       <body>{children}</body>
     </html>
