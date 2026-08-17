ef7fa05c86afa608e6388968e824c1734937ca43
aa89976566 <aa89976566@gmail.com>
2026-08-17 09:53:24 +0200

    Deploy final Taiwanese borderless posters v51

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <head>
-        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=side-borderless-tw-v50`} />
+        <link rel="stylesheet" href={`${asset("/haodada-site-v15.css")}?v=side-borderless-tw-v51`} />
       </head>
       <body>{children}</body>
     </html>
